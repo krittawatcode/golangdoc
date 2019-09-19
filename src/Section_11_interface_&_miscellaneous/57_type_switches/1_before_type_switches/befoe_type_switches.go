976a474a757ec2a221351b6387f61acfcc74aaae
Krittawat Wisedsuwon <krittawat.code@gmail.com>
2019-09-19 13:51:15 +0700

    add 57 type switches

--- src/Section_11_interface_&_miscellaneous/57_type_switches/1_before_type_switches/befoe_type_switches.go
+++ src/Section_11_interface_&_miscellaneous/57_type_switches/1_before_type_switches/befoe_type_switches.go
@@ -3,21 +3,27 @@ package main
 import "fmt"
 
 func testValue(x interface{}) {
-
-	switch v := x.(type) {
-	case string:
+	if v, ok := x.(string); ok {
 		fmt.Println("string value : ", v)
-	case int:
-		fmt.Println("int value : ", v)
-	case bool:
-		fmt.Println("bool value : ", v)
-	case float32:
-		fmt.Println("float32 value : ", v)
-	case float64:
-		fmt.Println("float64 value : ", v)
-	default:
-		fmt.Println("No match any")
+		return
 	}
+	if v, ok := x.(int); ok {
+		fmt.Println("int value : ", v)
+		return
+	}
+	if v, ok := x.(bool); ok {
+		fmt.Println("bool value : ", v)
+		return
+	}
+	if v, ok := x.(float32); ok {
+		fmt.Println("float32 value : ", v)
+		return
+	}
+	if v, ok := x.(float64); ok {
+		fmt.Println("float64 value : ", v)
+		return
+	}
+	fmt.Println("No match any")
 }
 
 func main() {
